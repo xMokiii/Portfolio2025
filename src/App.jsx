329ce4a2194c0f27756a12c3bc64abf87162d0ba
Colin MORLION <morlioncolin@gmail.com>
2025-04-07 21:52:02 +0200

    Apply .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import "./App.css";
 import { OrbitControls, Scroll, ScrollControls } from "@react-three/drei";
 import { Overlay } from "./components/Overlay";
 import { Office } from "./components/Office";
+import WaterEffect from './components/WaterEffect';
+import * as THREE from 'three';
 
 
 function App() {
+  const cubeTextureLoader = new THREE.CubeTextureLoader();
+  const environmentMap = cubeTextureLoader.load([
+    '/px.png', '/nx.png', '/py.png', '/ny.png', '/pz.png', '/nz.png',
+  ]);
   return (
     <Canvas
       camera={{
@@ -15,6 +21,7 @@ function App() {
     >
       <ambientLight intensity={1} />
       <OrbitControls enableZoom={false} />
+      <WaterEffect resolution={512} environmentMap={environmentMap} />
       <ScrollControls pages={3} damping={0.25}>
         <Overlay />
         <Office />
---
+++ src/components/WaterEffect.jsx
@@ -0,0 +1,51 @@
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
+import waterVertexShader from '../shaders/water.vert';
+import waterFragmentShader from '../shaders/water.frag';
+
+const WaterEffect = ({ resolution = 512, environmentMap }) => {
+  const waterRef = useRef();
+
+  const uniforms = {
+    uTime: { value: 0 },
+    uOpacity: { value: 0.8 },
+    uEnvironmentMap: { value: environmentMap },
+    uWavesAmplitude: { value: 0.025 },
+    uWavesFrequency: { value: 1.07 },
+    uWavesPersistence: { value: 0.3 },
+    uWavesLacunarity: { value: 2.18 },
+    uWavesIterations: { value: 8 },
+    uWavesSpeed: { value: 0.4 },
+    uTroughColor: { value: new THREE.Color('#186691') },
+    uSurfaceColor: { value: new THREE.Color('#9bd8c0') },
+    uPeakColor: { value: new THREE.Color('#bbd8e0') },
+    uPeakThreshold: { value: 0.08 },
+    uPeakTransition: { value: 0.05 },
+    uTroughThreshold: { value: -0.01 },
+    uTroughTransition: { value: 0.15 },
+    uFresnelScale: { value: 0.8 },
+    uFresnelPower: { value: 0.5 },
+  };
+
+  const material = new THREE.ShaderMaterial({
+    vertexShader: waterVertexShader,
+    fragmentShader: waterFragmentShader,
+    uniforms,
+    transparent: true,
+    depthTest: true,
+    side: THREE.DoubleSide,
+  });
+
+  const geometry = new THREE.PlaneGeometry(2, 2, resolution, resolution);
+
+  useFrame(({ clock }) => {
+    if (waterRef.current) {
+      uniforms.uTime.value = clock.getElapsedTime();
+    }
+  });
+
+  return <mesh ref={waterRef} material={material} geometry={geometry} rotation-x={-Math.PI / 2} />;
+};
+
+export default WaterEffect;
