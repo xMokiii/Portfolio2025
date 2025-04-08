dd0c323bdd2edcf4d7ab3c79f4ba86bf695866bd
Colin MORLION <morlioncolin@gmail.com>
2025-04-09 01:13:51 +0200

    Données relatives

--- src/components/CameraRig.jsx
+++ src/components/CameraRig.jsx
@@ -8,7 +8,7 @@ export default function CameraRig() {
   const { camera } = useThree();
   const scroll = useScroll();
 
-  // Stockage de l'état de la caméra manipulé par GSAP
+  // État de la caméra contrôlé par GSAP
   const gsapObj = useRef({
     x: camera.position.x,
     y: camera.position.y,
@@ -25,56 +25,73 @@ export default function CameraRig() {
   useEffect(() => {
     tl.current = gsap.timeline();
 
-    // Point d’intérêt 1
+    const { x, y, z, rotX, rotY, rotZ } = gsapObj.current;
+
+    // Point d’intérêt 1 (déplacement relatif)
     tl.current.to(gsapObj.current, {
       duration: 1,
-      x: 2.5,
-      y: -0.5,
-      z: -3,
-      rotX: 0,
-      rotY: 7,
-      rotZ: 0,
+      x: x ,
+      y: y - 0.2,
+      z: z - 2,
+      rotX: rotX + 0,
+      rotY: rotY + 6,
+      rotZ: rotZ + 0,
       ease: "power1.out",
     }, 0);
 
     // Pause
     tl.current.to({}, { duration: 0.5 }, 1);
 
-    // Point d’intérêt 2
+    // Point d’intérêt 2 (déplacement relatif)
     tl.current.to(gsapObj.current, {
       duration: 1,
-      x: 0,
-      y: -0.9,
-      z: -4.5,
-      rotX: 4,
-      rotY: -6,
-      rotZ: -3.75,
+      x: "+=0",
+      y: "-=0.4",
+      z: "-=1.5",
+      rotX: "+=4",
+      rotY: "-=13",
+      rotZ: "-=3.75",
       ease: "power1.out",
     }, 1.5);
 
     // Pause
     tl.current.to({}, { duration: 0.5 }, 2.5);
 
-    // Point d’intérêt 3
+    // Point d’intérêt 3 (déplacement relatif)
     tl.current.to(gsapObj.current, {
       duration: 1,
-      x: 0,
-      y: -1.9,
-      z: -4.5,
-      rotX: 4,
-      rotY: -6,
-      rotZ: -3.75,
+      x: "+=0",
+      y: "-=1",
+      z: "+=0",
+      rotX: "+=0",
+      rotY: "+=0",
+      rotZ: "+=0",
       ease: "power1.out",
     }, 3);
+
+    // Pause
+    tl.current.to({}, { duration: 0.5 }, 4);
+
+    // Point d’intérêt 4 (déplacement relatif)
+    tl.current.to(gsapObj.current, {
+      duration: 1,
+      x: "-=3",
+      y: "-=0.3",
+      z: "+=0.5",
+      rotX: "-=4",
+      rotY: "+=6",
+      rotZ: "+=3.75",
+      ease: "power1.out",
+    }, 4.5);
   }, []);
 
   useFrame(() => {
     if (!tl.current) return;
 
-    // Synchro du scroll avec la timeline
+    // Scroll synchronisé avec GSAP timeline
     tl.current.progress(scroll.offset);
 
-    // Interpolation vers la position cible
+    // Interpolation smooth vers la position/rotation cible
     targetPosition.current.set(
       gsapObj.current.x,
       gsapObj.current.y,
@@ -82,7 +99,6 @@ export default function CameraRig() {
     );
     camera.position.lerp(targetPosition.current, 0.1);
 
-    // Interpolation vers la rotation cible
     targetRotation.current.set(
       gsapObj.current.rotX,
       gsapObj.current.rotY,
